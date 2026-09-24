693bef875020d389214eef649c6d04bf6d70f037
zshuyaz <8jjn4y7zp7@privaterelay.appleid.com>
2026-09-24 13:59:03 +0800

    Create main.c

---
+++ EX1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\t\t2025 9月\n");
+    printf("日\t一\t二\t三\t四\t五\t六\n");
+    printf("\033[1;34m31");
+    printf("\033[1;36m\t 1\t 2\t 3\t 4\t 5\t 6\n");
+    printf(" 7\t 8\t 9\t10\t11\t12\t13\n");
+    printf("14\t15\t16\t17\t18\t19\t20\n");
+    printf("21\t22\t23\t24\t25\t26\t27\n");
+    printf("28\t29\t30\t");
+    printf("\033[1;34m 1\t 2\t 3\t 4\n");
+    return 0;
+}
